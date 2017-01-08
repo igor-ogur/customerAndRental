8c02e65fd7dd99d08211a5473908deaf0e85187f
igor-ogur <igor-ogur@mail.ru>
2017-01-09 00:17:13 +0200

    Runner changed

--- src/main/java/com/softgroup/test/Runner.java
+++ src/main/java/com/softgroup/test/Runner.java
@@ -2,7 +2,7 @@ package com.softgroup.test;
 
 public class Runner {
     public static void main( String[] args ){
-        Rental rental = new Rental(4, 4, 100);
+        Rental rental = new Rental(3, 4, 100);
         Customer customer1 = new Customer("Ivan", rental);
 
         System.out.println(customer1.getName() +" "+ customer1.getRental().amountFor());
